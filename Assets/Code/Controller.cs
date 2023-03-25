bca04c6d5736e40357d8ed5a6e1e44912f860893
Ian Badcoe <igbadcoe@gmail.com>
2023-03-25 12:52:13 +0000

    Basic demo of zooming/rotating/fading in and out...

--- Assets/Code/Controller.cs
+++ Assets/Code/Controller.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Code
 {
@@ -9,31 +12,72 @@ namespace Code
         public GameObject Container;
         public Image Background;
         public Color PrevBackgroundColour;
+        public float StartDelay;
+
+        float StartTime;
+        float LastTime;
 
         // Use this for initialization
         void Start()
         {
-            var first = true;
+            // "param" is going to be a function of time, maybe just linear time
+            // but for clarity keep the name separate
+            float cumulative_start_param = -1;
 
-            foreach(Transform t in Container.transform)
+            List<Transform> transforms = new List<Transform>();
+
+            foreach (Transform t in Container.transform)
             {
                 var z = t.GetComponent<Zoomer>();
 
                 if (z != null)
                 {
-                    if (first)
+                    if (cumulative_start_param == -1)
                     {
-                        z.SetInterp(1);
-                        PrevBackgroundColour = z.BackgroundColour;
-                    }
-                    else
-                    {
-                        z.SetInterp(0);
+                        // make the start delay happen fully before the initial fade in
+                        // later fades overlap the preceding period
+                        cumulative_start_param = StartDelay + z.InLength;
                     }
 
-                    first = false;
+                    z.SetParamOffset(cumulative_start_param);
+
+                    // add out total live time to the running total
+                    cumulative_start_param += z.LiveLength;
+
+                    transforms.Insert(0, t);
+                }
+            }
+
+            foreach(Transform t in transforms)
+            {
+                t.SetParent(transform);
+                t.SetParent(Container.transform);
+            }
+
+            StartTime = Time.time;
+        }
+
+        void Update()
+        {
+            LastTime = Time.time - StartTime;
+
+            foreach (Transform t in Container.transform)
+            {
+                var z = t.GetComponent<Zoomer>();
+
+                if (z != null)
+                {
+                    // param == time at the moment...
+                    z.SetInterp(LastTime);
                 }
             }
         }
+
+        //private void OnGUI()
+        //{
+        //    var style = new GUIStyle();
+        //    style.fontSize = 32;
+        //    Handles.Label(new Vector3(Screen.width - 400, Screen.height - 200, 0), $"{LastTime}", style);
+        //}
     }
 }
--- Assets/Code/Zoomer.cs
+++ Assets/Code/Zoomer.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +8,50 @@ namespace Code
 {
     public class Zoomer : MonoBehaviour
     {
+        [Header("Our Objects")]
         public GameObject Centre;
         public CanvasGroup CanvasGroup;
-        public bool UseBackgroundColour = false;
-        public Color BackgroundColour = Color.red;
 
+        [Header("Interp Config")]
+        public float InLength;
+        public float LiveLength;
+        public float OutLength;
+
+        [Header("Appearance Params")]
+        //public bool UseBackgroundColour = false;
+        //public Color BackgroundColour = Color.red;
+        public float RotationRate = 1;
+        public float ScaleRate = 1;
+
+        public enum Phase
+        {
+            Unknown,
+
+            Before,
+            In,
+            Live,
+            Out,
+            After
+        }
+
+        public Phase CurrentPhase { get; private set; } = Phase.Unknown;
+
+        // run params
+        // ParamOffset is the param where we finish fading-in...
+        float ParamOffset;
         Controller Controller;
+
+        // convenience
+        bool Running => CurrentPhase != Phase.Before && CurrentPhase != Phase.After;
+        float StartInParam => ParamOffset - InLength;
+        float EndInParam => ParamOffset;
+        float StartOutParam => ParamOffset + LiveLength;
+        float EndOutParam => StartOutParam + OutLength;
+
+        Tweener Tween;
+
+        float LastParam = -1;
+        float InitRotation;
 
         void Start()
         {
@@ -20,27 +60,172 @@ namespace Code
             while (p != null && Controller == null)
             {
                 Controller = p.GetComponent<Controller>();
+                p = p.parent;
             }
 
             Debug.Assert(Controller != null);
+
+            InitRotation = transform.rotation.eulerAngles.y;
+
+            // we may have things set wrongly in the overall appearance, to allow easy editing
+            // e.g. not _invisible_ but this sets us to the correct sart-of-run state
+            Initialise();
+        }
+
+        private void Initialise()
+        {
+            CanvasGroup.alpha = 0;
+        }
+
+        // anatomy of a ZoomSegment:
+        //
+        //                       InLength    +---LiveLength----+   OutLength
+        //                               \  /|                 |\ /
+        //                                 / |                 | \
+        //                                /  |                 |  \
+        // 0 (e.g. invisible) <----------+   |                 |   +---------->
+        //                                   |
+        //                                   |
+        //                                   |
+        //                        ParamOffset 
+        //
+        // Calling SetOffset adjusts EndInParam (e.g. the start of the full-displayed section)
+        // to new_end_in_param and also adjusts the other three params by the same relative change
+        public void SetParamOffset(float offset)
+        {
+            ParamOffset = offset;
         }
 
         // interp from 0-1 is the "in" process
         //        from 1-2 is the "out" process
-        public void SetInterp(float interp)
+        public void SetInterp(float param)
         {
-            if (interp < 1)
+            if (param < StartInParam)
             {
-                CanvasGroup.alpha = interp;
+                if (Running)
+                {
+                    Reset();
+                }
+
+                CurrentPhase = Phase.Before;
+
+                return;
             }
-            else if (interp == 1)
+            else if (param > EndOutParam)
             {
-                Controller.PrevBackgroundColour = BackgroundColour;
+                if (Running)
+                {
+                    Reset();
+                }
+
+                CurrentPhase = Phase.After;
+
+                return;
             }
-            else
+
+            if (param < EndInParam)
             {
-                CanvasGroup.alpha = 2 - interp;
+                if (CurrentPhase != Phase.In)
+                {
+                    Reset();
+
+                    Tween = InitIn(param);
+                }
             }
+            else if (param < StartOutParam)
+            {
+                if (CurrentPhase != Phase.Live)
+                {
+                    Reset();
+
+                    Tween = InitLive(param);
+                }
+            }
+            else if (param < EndOutParam)
+            {
+                if (CurrentPhase != Phase.Out)
+                {
+                    Reset();
+
+                    Tween = InitOut(param);
+                }
+            }
+
+            float delta_param = param - LastParam;
+            LastParam = param;
+
+            Tween.ManualUpdate(delta_param, delta_param);
+        }
+
+        private Tweener InitIn(float param)
+        {
+            CurrentPhase = Phase.In;
+            LastParam = StartInParam;
+            return DOTween.To(Interpolate, 0, InLength, InLength)
+                .SetEase(Ease.Linear)
+                .SetUpdate(UpdateType.Manual);
+        }
+
+        private Tweener InitLive(float param)
+        {
+            CurrentPhase = Phase.Live;
+            LastParam = EndInParam;
+            return DOTween.To(Interpolate, 0, LiveLength, LiveLength)
+                .SetEase(Ease.Linear)
+                .SetUpdate(UpdateType.Manual);
+        }
+
+        private Tweener InitOut(float param)
+        {
+            CurrentPhase = Phase.Out;
+            LastParam = StartOutParam;
+            return DOTween.To(Interpolate, 0, OutLength, OutLength)
+                .SetEase(Ease.Linear)
+                .SetUpdate(UpdateType.Manual);
+        }
+
+        private void Interpolate(float interp)
+        {
+            float alpha = 1;
+            float scale = 1;
+            float rotation = InitRotation;
+
+            switch (CurrentPhase)
+            {
+                case Phase.In:
+                    alpha = interp / InLength;
+                    rotation = InitRotation + interp;
+                    scale = interp / InLength;
+                    break;
+
+                case Phase.Live:
+                    rotation = InitRotation + InLength + interp;
+                    scale = 1 + interp;
+                    break;
+
+                case Phase.Out:
+                    alpha = 1 - interp / OutLength;
+                    rotation = InitRotation + InLength + LiveLength + interp;
+                    scale = 1 + LiveLength + interp / OutLength;
+                    break;
+            }
+
+            CanvasGroup.alpha = alpha;
+            transform.rotation = Quaternion.Euler(0, 0, rotation * RotationRate);
+            scale *= ScaleRate;
+            transform.localScale = new Vector3(scale, scale, scale);
+        }
+
+        private void Reset()
+        {
+            if (Tween != null)
+            {
+                Tween.Complete();
+            }
+
+            Tween = null;
+
+            CurrentPhase = Phase.Unknown;
         }
     }
 }

59058bd46620bdcfaf7f5fc59741f65a3ae330fb
Ian Badcoe <igbadcoe@gmail.com>
2023-04-16 20:00:01 +0100

    Paper zooming, 20+ example types of paper

--- Assets/Code/Controller.cs
+++ Assets/Code/Controller.cs
@@ -12,8 +12,6 @@ namespace Code
     public class Controller : MonoBehaviour
     {
         public GameObject Container;
-        public Image Background;
-        public Color PrevBackgroundColour;
         public PaperZoomer PaperZoomer;
 
         // just public for ease of debugging...
@@ -23,44 +21,9 @@ namespace Code
         bool Running = false;
         bool Escaping = false;
 
-        public Transform TrackingTransform1;
-        public Transform TrackingTransform2;
-        public float TrackFrac1 = 0;
-        public float TrackFrac2 = 0;
-        public float TrackRate = 0.02f;
-
         // Use this for initialization
         void Start()
         {
-            // zoom is going to be a function of time, maybe just linear time
-            // but for clarity keep the name separate
-            float cumulative_start_zoom = 0;
-
-            Zoomer prev_layer = null;
-
-            int index = 0;
-
-            foreach (Transform t in Container.transform)
-            {
-                var z = t.GetComponent<Zoomer>();
-
-                if (z != null && z.gameObject.activeInHierarchy)
-                {
-                    // add our total live time to the running total
-                    cumulative_start_zoom += z.SetZoomOffset(cumulative_start_zoom,
-                        (RectTransform)prev_layer?.ChildLocation.transform,
-                        ((RectTransform)Container.transform).rect,
-                        index);
-
-                    index += 1;
-                    prev_layer = z;
-
-                    if (TrackingTransform1 == null)
-                    {
-                        TrackingTransform1 = z.transform;
-                    }
-                }
-            }
         }
 
         void Update()
@@ -91,66 +54,7 @@ namespace Code
                 RelativeTime += Time.deltaTime * TimeRate;
 
                 PaperZoomer.SetZoom(RelativeTime);
-
-                foreach (Transform t in Container.transform)
-                {
-                    var z = t.GetComponent<Zoomer>();
-
-                    if (z != null && t.gameObject.activeInHierarchy)
-                    {
-                        // zoom == time at the moment...
-                        z.SetZoom(RelativeTime);
-                    }
-                }
-
-                if (TrackingTransform1 != null)
-                {
-                    TrackFrac1 += TrackRate;
-                    if (TrackFrac1 > 1)
-                    {
-                        TrackFrac1 = 1;
-                    }
-
-                    float interp1 = DG.Tweening.DOVirtual.EasedValue(0, 1, TrackFrac1, Ease.InCubic);
-
-                    // We want to get LocationTransform to 0, 0 so we shift the whole container, negatively, by
-                    // a proportion of where it currently is...
-
-                    Container.transform.position -= TrackingTransform1.position * interp1;
-                }
-
-                if (TrackingTransform2 != null) {
-                    float interp2 = DOVirtual.EasedValue(0, 1, TrackFrac2, Ease.InCubic);
-
-                    Container.transform.position -= TrackingTransform2.position * interp2;
-
-                    TrackFrac2 -= TrackRate;
-                    if (TrackFrac2 < 0)
-                    {
-                        TrackFrac2 = 0;
-                        TrackingTransform2 = null;
-                    }
-                }
-
-                // move the paper in unity with the zoomers/text
-                PaperZoomer.transform.position = Container.transform.position;
             }
         }
-
-        internal void SetTrackingTransform(RectTransform t)
-        {
-            TrackingTransform2 = TrackingTransform1;
-            TrackFrac2 = TrackFrac1;
-
-            TrackingTransform1 = t;
-            TrackFrac1 = 0;
-        }
-
-        //private void OnGUI()
-        //{
-        //    var style = new GUIStyle();
-        //    style.fontSize = 32;
-        //    Handles.Label(new Vector3(Screen.width - 400, Screen.height - 200, 0), $"{LastTime}", style);
-        //}
     }
 }
--- Assets/Code/PaperZoomer.cs
+++ Assets/Code/PaperZoomer.cs
@@ -3,122 +3,84 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Code
 {
     public class PaperZoomer : MonoBehaviour
     {
-        [Header("Our Objects")]
-        public CanvasGroup[] CGs = new CanvasGroup[2];
-        public float[] ZoomOffsets = new float[2];
-        public float ZoomLimit;
-        public float Flip = -1;
+        public GameObject PaperTemplate;
+        public Sprite[] Sprites;
+
+        GameObject[] Layers;
+
+        private int NumFrames => Sprites.Length;
 
         void Start()
         {
-            // we are going to zoom each CG by 2x and then jump back to what should be a perfect loop-point at 1/2 the size...
-            //
-            //  Time    CG-0            CG-1
-            //          scale   alpha   scale   alpha
-            //  0       1       0       2       1
-            //  0.5     1.5     0.5     3       0.5
-            //  1       2       1       4       0
-            //
-            // (OK, so I am not using the exact time or alpha there, but the 0.5 line just means (part way)
-            //
-            // so what happens is that the alpha of CGs[0] doesn't actually change, because it is the rear one and its effective alpha
-            // is achieved by the change to CGs[1] in front of it...
-            //
-            // So at t = 0, CG-0 completely hidden behind fully-opaque CG-1 with a scale of 2 -> 2x paper visible
-            // Between t = 0 and t = 1, CG-1 fades out and scales to 2x -> a mix of >1 scale and >2 scale paper
-            // At t = 1, CG-1 is completely transparent, leaving CG-0 visible with a scale of 2 -> 2x paper visible
-            //
-            // So the end looks just like the start and we can begin again
+            Layers = new GameObject[NumFrames];
 
-            CGs[0].alpha = 1;
+            int idx = 0;
 
-            Reset(0);
+            foreach(var sprite in Sprites)
+            {
+                GameObject go = Instantiate(PaperTemplate, transform);
+
+                Image img = go.GetComponent<Image>();
+                img.sprite = sprite;
+
+                // -ve values seem to be ignored here
+                go.GetComponent<Canvas>().sortingOrder = /*NumFrames -*/ idx;
+
+                Layers[idx] = go;
+
+                idx++;
+            }
+
+            SetZoom(0);
         }
 
-        private void Reset(float param)
-        {
-            ZoomLimit = MathF.Log10(2);
-            ZoomOffsets[0] = param;
-            ZoomOffsets[1] = param - ZoomLimit;
-        }
+        // zoom = 0 means CGs[0] is at scale 2 (about to start fading out)
+        //      and CGs[1] is at scale 1 (about to start becoming visible because the other is fading out)
+        // zoom = 1 means CGs[0] is at scale 4 (alpha = 0.5),
+        //      CGs[1] is at scale 2 (about to start fading out)
+        //      and CGs[2] is at scale 1 (about to start getting revealed)
+        // zoom = 2 means CGs[0] is at scale 8 (alpha = 0, just become completely invisible)
+        //      CGs[1] is atg scale 4 (alpha = 0.5)
+
+        // param    scale-0 alpha-0 scale-1 alpha-1 scale-2 alpha-2 scale-3 alpha-3  ...     scale-N alpha-N
+        // 0        2       1       1       1       -       -       -       -        ...     4       0
+        // 0.5      2.5     0.5     1.5     1       -       -       -       -        ...     4       0
+        // 1        4       0.5     2       1       1       1       -       -        ...     -       -
+        // 2        -       -       4       0.5     2       1       1       -        ...     -       -
+        // 3        0       0       0       0       4       0       2       0.5      ...     -       -
+        //
+        // N        2       1       1       1       -       -       -       -        ...     4       0
 
         public void SetZoom(float param)
         {
-            float rel_param0 = param - ZoomOffsets[0];
-            float rel_param1 = param - ZoomOffsets[1];
+            int idx = 0;
 
-            // using x4 instead of x2 allows us a bit of leeway to move the paper around...
-            float scale0 = Mathf.Pow(10, rel_param0) * 4 * Flip;
-            float scale1 = Mathf.Pow(10, rel_param1) * 4 * -Flip;          // rotating 180 just makes it look different...
-
-            CGs[0].transform.localScale = new Vector3(scale0, scale0, 1);
-            CGs[1].transform.localScale = new Vector3(scale1, scale1, 1);
-            CGs[1].alpha = 1 - (rel_param1 / ZoomLimit);
-
-            if (rel_param1 >= ZoomLimit)
+            foreach (var layer in Layers)
             {
-                Reset(ZoomOffsets[0] + ZoomLimit);
-                Flip = -Flip;
+                float rel_param = param - idx;
+
+                // so at param = 0, rel_param is 0 for CGs[0], -1 for CGs[1]
+
+                // now, at param = 0, for CGs[NumFrames - 1], rel_param is 2
+
+                // when we hit rel_param = 0, we want to be at scale = 2, so that as we fade out the layer behind us
+                // is already at scale = 1 and doesn't have any gaps around the edge...
+                var scale = Mathf.Pow(2, rel_param) /** 2*/;
+
+                layer.transform.localScale = new Vector3(scale, scale, 1);
+
+                var alpha = Mathf.Clamp01(rel_param + 1/*/ 2*/);
+
+                layer.GetComponent<CanvasGroup>().alpha = alpha;
+
+                idx++;
             }
-
-        //    if (Done)
-        //    {
-        //        return true;
-        //    }
-
-        //    var ret = false;
-
-        //    if (param < StartInParam)
-        //    {
-        //        return false;
-        //    }
-
-        //    if (!Started)
-        //    {
-        //        Started = true;
-
-        //        if (LocationTransform != null)
-        //        {
-        //            Controller.SetTrackingTransform(LocationTransform);
-        //        }
-        //    }
-
-        //    if (param >= EndLiveParam)
-        //    {
-        //        ret = true;
-        //    }
-
-        //    // this is 0 at the EndInParam and -ve before that...
-        //    // meaning we are smaller before the end (scale < 1), and
-        //    // bigger after (scale > 1)
-        //    float rel_param = param - EndInParam;
-        //    var curr_scale = Mathf.Pow(10, rel_param);
-
-        //    // we really shouldn't be required after reaching this size
-        //    if (curr_scale > 100)
-        //    {
-        //        SetDone(true);
-        //    }
-
-        //    var hrt = (RectTransform)transform;
-
-        //    hrt.localScale = new Vector3(curr_scale, curr_scale, 1);
-        //    // 0 at StartInParam, 1 at EndInParam
-
-        //    float in_frac = (param - StartInParam) / InLength;
-        //    CanvasGroup.alpha = Mathf.Clamp01(in_frac);
-
-        //    if (LocationTransform != null)
-        //    {
-        //        hrt.position = LocationTransform.position;
-        //    }
-
-        //    return ret;
         }
     }
 }
